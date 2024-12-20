6445d4442c09c173a2be1f1e1f71488932abd1da
guojb0820 <guojb0820foxmail.com>
2024-12-20 14:27:28 +0800

    添加SafePassword文件夹和里面的1_encryption.ps1、2_use.ps1文件

---
+++ SafePassword/1_encryption.ps1
@@ -0,0 +1,24 @@
+﻿#账号密码
+#$adminUser = "xazj\it"
+#$adminPassword = "Elite123@@"
+#转换为安全字符
+#$password = ConvertTo-SecureString $adminPassword -AsPlainText -Force
+
+#手动输入，避免明文
+#$adminUser = Read-Host "Enter the administrator username"
+#$adminPassword = Read-Host "Enter the password" -AsSecureString
+
+
+# 创建PSCredential对象
+$credential = New-Object System.Management.Automation.PSCredential ($adminUser, $adminPassword)
+# 将密码转换为加密格式
+$encryptedPassword = ConvertFrom-SecureString $credential.Password
+# 保存加密后的密码到文件
+$encryptedPassword | Out-File -FilePath "C:\efi\update\encryptedCredential.txt"
+
+
+
+
+
+
+
---
+++ SafePassword/2_use.ps1
@@ -0,0 +1,21 @@
+﻿# 从文件加载加密的密码
+$encryptedPassword = Get-Content "C:\efi\update\encryptedCredential.txt" | ConvertTo-SecureString
+
+# 输入之前使用的用户名
+$adminUser = "xazj\administrator"
+
+# 用解密后的密码和用户名创建PSCredential对象
+$credential = New-Object System.Management.Automation.PSCredential ($adminUser, $encryptedPassword)
+
+# 使用凭据运行命令
+Start-Process -FilePath "C:\Program Files (x86)\Feishu\uninstall.exe" -Credential $credential
+
+#$session = New-PSSession -ComputerName "localhost" -Credential $credential
+
+# 在会话中执行命令
+#Invoke-Command -Session $session -ScriptBlock {
+#    Start-Process -FilePath "C:\efi\feishu.exe" -Credential $credential -Wait
+#}
+
+# 关闭会话
+#Remove-PSSession -Session $session
